2f62dfd9436f824e49881e8149f5833fce41aa44
Apheksha B <apheksha.b@gmail.com>
2026-05-02 23:15:36 +0530

    Enhance echoall.c to display environment variables

--- Question 12/partb/echoall.c
+++ Question 12/partb/echoall.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
 
+extern char **environ;
+
 int main(int argc, char *argv[])
 {
-	int i;
+    int i;
 
-	for(i = 0;i<argc;i++)
-	{
-		printf("argv[%d]:%s\n", i , argv[i]);
-	}
-	return 0;
+    printf("Arguments:\n");
+    for(i=0;i<argc;i++)
+        printf("argv[%d]=%s\n", i, argv[i]);
+
+    printf("\nEnvironment variables:\n");
+    for(i=0; environ[i]!=NULL; i++)
+        printf("%s\n", environ[i]);
+
+    return 0;
 }
