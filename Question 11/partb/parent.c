a81ad79381afe61342a1ed9dfa32cc1e2e08348f
Apheksha B <apheksha.b@gmail.com>
2026-05-02 23:06:17 +0530

    Refactor parent process handling and error reporting

--- Question 11/partb/parent.c
+++ Question 11/partb/parent.c
@@ -1,27 +1,31 @@
-
 #include<stdio.h>
 #include<unistd.h>
+#include<stdlib.h>
 #include<sys/wait.h>
-#include<fcntl.h>
 
 int main(int argc, char *argv[])
 {
-	int pid, status;
+    int pid, status;
 
-	if(argc != 3)
-	{
-		printf("usage :%s num1 num2\n",argv[0]);
-		return 1;
-	}
-	pid = fork();
-	if(pid == 0)
-	{
-		execl("./child","child",argv[1],argv[2],NULL);
-		printf("Exel failed");
-	}
-	else{
-		wait(&status);
-		printf("Child exited with status: %d\n", status);
-	}
-	return 0;
+    if(argc != 3)
+    {
+        printf("Usage: %s num1 num2\n", argv[0]);
+        return 1;
+    }
+
+    pid = fork();
+
+    if(pid == 0)
+    {
+        execl("./child", "child", argv[1], argv[2], NULL);
+        perror("execl");
+        exit(1);
+    }
+    else
+    {
+        wait(&status);
+        printf("Child exited with status: %d\n", WEXITSTATUS(status));
+    }
+
+    return 0;
 }
