7515e8cce26cb11fbd1704241a2ba8bb226dbce3
Apheksha B <apheksha.b@gmail.com>
2026-05-02 18:59:36 +0530

    Refactor SIGINT handler for clarity and correctness
    Updated signal handler to provide clearer message and use sigaction correctly.

--- Question 5/pro2.c
+++ Question 5/pro2.c
@@ -1,10 +1,18 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<signal.h>
+#include<unistd.h>
 
-void handler(int n)
+void handler(int signo)
 {
-    printf("\nSIGINT caught\n");
-    signal(SIGINT, SIG_DFL);
+    printf("\nSIGINT caught. Next Ctrl+C will terminate.\n");
+
+    struct sigaction sa;
+    sa.sa_handler = SIG_DFL;          
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+
+    sigaction(SIGINT, &sa, NULL);
 }
 
 int main()
@@ -12,9 +20,19 @@ int main()
     struct sigaction sa;
 
     sa.sa_handler = handler;
+    sigemptyset(&sa.sa_mask);
     sa.sa_flags = 0;
 
-    sigaction(SIGINT, &sa, NULL);
+    if(sigaction(SIGINT, &sa, NULL) < 0)
+    {
+        perror("sigaction");
+        return 1;
+    }
 
-    while(1);
+    printf("Press Ctrl+C to trigger SIGINT\n");
+
+    while(1)
+        pause();   
+
+    return 0;
 }
