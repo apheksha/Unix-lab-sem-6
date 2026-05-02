d3dce06f719e29caf37d283e994f6cdbaa3c2de4
Apheksha B <apheksha.b@gmail.com>
2026-05-02 18:18:52 +0530

    Update link creation logic in pro1.c
    Refactor link creation logic to differentiate between hard and soft links based on command line arguments.

--- Question 4/pro1.c
+++ Question 4/pro1.c
@@ -1,29 +1,29 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<sys/stat.h>
-#include<sys/types.h>
-#include<fcntl.h>
 #include<unistd.h>
 
 int main(int argc, char *argv[])
 {
-	if(argc == 3)
-	{
-		if(link(argv[1],argv[2]) == 0)
-			printf("Hard link created\n");
-		else
-			printf("Hard link error");
-	}
-	else if(argc == 4)
-	{
-		if(symlink(argv[2], argv[3]) == 0)
-			printf("Soft link created");
-		else 
-			printf("soft link error");
-	}
-	else
-	{
-		printf("Invalid argument");
-	}
-	return 0;
+    if(argc == 4 && argv[1][0] == 'h')
+    {
+        if(link(argv[2], argv[3]) == 0)
+            printf("Hard link created\n");
+        else
+            perror("link");
+    }
+    else if(argc == 4 && argv[1][0] == 's')
+    {
+        if(symlink(argv[2], argv[3]) == 0)
+            printf("Soft link created\n");
+        else
+            perror("symlink");
+    }
+    else
+    {
+        printf("Usage:\n");
+        printf("%s h <source> <hardlink>\n", argv[0]);
+        printf("%s s <source> <softlink>\n", argv[0]);
+    }
+
+    return 0;
 }
