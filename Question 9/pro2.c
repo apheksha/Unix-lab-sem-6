aea1862e16c51fcee612202c723c7ebc64a0cbae
Apheksha B <apheksha.b@gmail.com>
2026-05-02 21:18:15 +0530

    Refactor pro2.c for improved error handling and output

--- Question 9/pro2.c
+++ Question 9/pro2.c
@@ -1,45 +1,45 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<fcntl.h>
-#include<sys/wait.h>
 #include<sys/types.h>
-#include<sys/stat.h>
 
 int main(int argc, char *argv[])
 {
-	int fd;
-	char buf[21];
-	int n;
+    int fd;
+    char buf[21];
+    ssize_t n;
+    off_t size;
 
-	if(argc != 2)
-	{
-		printf("usage source destination\n",argv[0]);
-		return 1;
-	}
+    if(argc != 2)
+    {
+        printf("Usage: %s filename\n", argv[0]);
+        return 1;
+    }
 
-	fd = open(argv[1],O_RDONLY);
-	if(fd < 0)
-	{
-		printf("error opening file");
-		return 1;
-	}
+    fd = open(argv[1], O_RDONLY);
+    if(fd < 0)
+    {
+        perror("open");
+        return 1;
+    }
 
-	n = read(fd, buf, 20);
-	buf[n] = '\0';
-	printf("first 20: %s\n", buf);
+    n = read(fd, buf, 20);
+    if(n < 0) { perror("read"); return 1; }
+    buf[n] = '\0';
+    printf("First 20 chars: %s\n", buf);
 
-	lseek(fd, 10, SEEK_SET);
-	n = read(fd, buf, 20);
-	buf[n] = '\0';
-	printf("Next 20 from 10: %s\n", buf);
-	
-	lseek(fd, 10, SEEK_CUR);
-	n = read(fd, buf, 20);
-	buf[n] = '\0'; 
-	printf("next 20,from current: %s\n", buf);
-	
-	int size = lseek(fd, 0, SEEK_END);
-	printf("file size:%d\n", size);
+    lseek(fd, 10, SEEK_SET);
+    n = read(fd, buf, 20);
+    buf[n] = '\0';
+    printf("Next 20 from 10th byte: %s\n", buf);
+
+    lseek(fd, 10, SEEK_CUR);
+    n = read(fd, buf, 20);
+    buf[n] = '\0';
+    printf("Next 20 from current+10: %s\n", buf);
+
+    size = lseek(fd, 0, SEEK_END);
+    printf("File size: %ld bytes\n", (long)size);
 
     close(fd);
     return 0;
