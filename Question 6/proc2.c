67fd48eb3d15831354643834035f83894d4d8930
Apheksha B <apheksha.b@gmail.com>
2026-05-02 19:58:58 +0530

    Add file locking and read last 50 bytes functionality
    Implement file locking mechanism to read last 50 bytes.

---
+++ Question 6/proc2.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+int main() {
+	int fd,n;
+	char f1[100],buf[51];
+	struct flock f;
+	off_t s;
+	printf("Enter file name: ");
+	scanf("%s",f1);
+	fd=open(f1,O_RDWR);
+
+	s=lseek(fd,0,SEEK_END);
+
+	f.l_type=F_WRLCK;
+	f.l_whence=SEEK_SET;
+	f.l_start=s-100;
+	f.l_len=100;
+
+	fcntl(fd,F_GETLK,&f);
+	if(f.l_type!=F_UNLCK) {
+		printf("Locked by PID: %d\n",f.l_pid);
+		return 0;
+	}
+
+	f.l_type=F_WRLCK;
+	fcntl(fd,F_SETLK,&f);
+
+	printf("Region locked. Press Enter to continue...\n");
+	getchar();
+	getchar();
+
+	lseek(fd,s-50,SEEK_SET);
+	n=read(fd,buf,50);
+	buf[n]='\0';
+	printf("%s\n",buf);
+
+	f.l_type=F_UNLCK;
+	fcntl(fd,F_SETLK,&f);
+
+	printf("Region unlocked\n");
+}
