6f5d2423096fc8b2ba5251928029261327f5070b
Apheksha B <apheksha.b@gmail.com>
2026-05-02 22:34:57 +0530

    Refactor process creation and wait handling

--- Question 10/pro2.c
+++ Question 10/pro2.c
@@ -1,33 +1,38 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<sys/wait.h>
 
-int main(void) {
-    pid_t c1, c2;
+int main()
+{
+    pid_t p1, p2;
     int status;
 
-    c1 = fork();
-    if (c1 == 0) {
+    p1 = fork();
+
+    if(p1 == 0)
+    {
         printf("Child 1 PID: %d\n", getpid());
         sleep(2);
         exit(1);
     }
 
-    c2 = fork();
-    if (c2 == 0) {
+    p2 = fork();
+
+    if(p2 == 0)
+    {
         printf("Child 2 PID: %d\n", getpid());
-        sleep(4);
+        sleep(1);
         exit(2);
     }
 
-    waitpid(c1, &status, 0);
-    printf("waitpid collected child 1 (PID %d), status: %d\n",
-           c1, WEXITSTATUS(status));
+    printf("Parent PID: %d\n", getpid());
+
+    waitpid(p1, &status, 0);
+    printf("waitpid: Child 1 finished\n");
 
     wait(&status);
-    printf("wait collected child 2, status: %d\n",
-           WEXITSTATUS(status));
+    printf("wait: Remaining child finished\n");
 
     return 0;
 }
