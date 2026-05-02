6aa82134a3f877b8c46185707d6a4d319424ced4
Apheksha B <apheksha.b@gmail.com>
2026-05-02 21:42:16 +0530

    Update pro2.c

--- Question 10/pro2.c
+++ Question 10/pro2.c
@@ -1,37 +1,33 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<fcntl.h>
-#include<stdlib.h>
-#include<sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 
+int main(void) {
+    pid_t c1, c2;
+    int status;
 
-int main()
-{
-	int pd1,pd2;
+    c1 = fork();
+    if (c1 == 0) {
+        printf("Child 1 PID: %d\n", getpid());
+        sleep(2);
+        exit(1);
+    }
 
-	pd1 = fork();
-	if(pd1 == 0)
-	{
-		printf("First pid :%d\n",getpid());
-		sleep(1);
-		printf("First wait\n");
-		return 0;
-	}
-	else{
-		pd2 = fork();
-		if(pd2 == 0)
-		{
-			printf("Second pid :%d\n", getpid());
-			sleep(2);
-			printf("Second wait\n");
-			return 0;
-		}
-		else{
-			printf("first pid :%d\n", getpid());
+    c2 = fork();
+    if (c2 == 0) {
+        printf("Child 2 PID: %d\n", getpid());
+        sleep(4);
+        exit(2);
+    }
 
-			wait(NULL);
-			waitpid(pd2, NULL, 0);
-		}
-	}
-	 return 0;
+    waitpid(c1, &status, 0);
+    printf("waitpid collected child 1 (PID %d), status: %d\n",
+           c1, WEXITSTATUS(status));
+
+    wait(&status);
+    printf("wait collected child 2, status: %d\n",
+           WEXITSTATUS(status));
+
+    return 0;
 }
