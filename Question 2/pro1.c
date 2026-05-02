8d96144443808145cac6d5f2cb27b39415d1655f
Apheksha B <apheksha.b@gmail.com>
2026-05-02 16:56:42 +0530

    Cast struct fields to long for printf compatibility

--- Question 2/pro1.c
+++ Question 2/pro1.c
@@ -17,9 +17,10 @@ int main()
 	}
 
 	printf("\nFile Name         :  %s\n", file);
-	printf("File Size         : %ld bytes\n", s.st_size);
+	printf("File Size         : %ld bytes\n", (long)s.st_size);
+	printf("Inode number : %ld\n", (long)s.st_ino);
 	printf("Permissions       : %o\n" , s.st_mode & 0777);
-	printf("Number of links   : %ld\n", s.st_nlink);
+	printf("Number of links   : %ld\n", (long)s.st_nlink);
 	printf("Owner UID         : %d\n", s.st_uid);
 	printf("Owner GID         : %d\n", s.st_gid);
 	printf("Last Access Time  : %s",ctime(&s.st_atime));
