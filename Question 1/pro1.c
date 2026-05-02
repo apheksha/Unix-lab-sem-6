b096b97abc3e3fdf3ddb48cfde7156c3cf096c37
Apheksha B <apheksha.b@gmail.com>
2026-05-02 17:41:34 +0530

    Refactor file reading with better error handling
    Improved error handling and increased buffer size for filename input.

--- Question 1/pro1.c
+++ Question 1/pro1.c
@@ -1,24 +1,43 @@
 #include<stdio.h>
 #include<fcntl.h>
 #include<unistd.h>
-int main(){
-	char file[20], ch;
-	int fd,size,i;
-	printf("Enter the filename:");
-	scanf("%s", file);
-	fd = open(file,O_RDONLY);
+#include<stdlib.h>
 
-	if(fd < 0){
-		printf("File not found");
-		return 0;
-	}
-	size = lseek(fd, 0, SEEK_END);
-	printf("Reverse Content:\n");
-	
-	for(int i=size-1; i>=0;i--){
-		lseek(fd, i, SEEK_SET);
-		read(fd, &ch, 1);
-		printf("%c", ch);
-	}
-	close(fd);
+int main(){
+    char file[100], ch;
+    int fd;
+    off_t size, i;
+
+    printf("Enter the filename: ");
+    scanf("%s", file);
+
+    fd = open(file, O_RDONLY);
+
+    if(fd < 0){
+        perror("open");
+        return 1;
+    }
+
+    size = lseek(fd, 0, SEEK_END);
+    if(size < 0){
+        perror("lseek");
+        close(fd);
+        return 1;
+    }
+
+    printf("Reverse Content:\n");
+
+    for(i = size - 1; i >= 0; i--){
+        lseek(fd, i, SEEK_SET);
+        if(read(fd, &ch, 1) != 1){
+            perror("read");
+            close(fd);
+            return 1;
+        }
+        printf("%c", ch);
+    }
+
+    printf("\n");
+    close(fd);
+    return 0;
 }
