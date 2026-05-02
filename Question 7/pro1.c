afcabb4d68fcd069967dcde841055a866f7e54af
Apheksha B <apheksha.b@gmail.com>
2026-05-02 20:12:38 +0530

    Refactor f1 function for better readability

--- Question 7/pro1.c
+++ Question 7/pro1.c
@@ -7,30 +7,32 @@ static int sv = 99;
 
 void f1()
 {
-	int av = 96;
-	register int rv = 97;
-	volatile int vv = 98;
+    int av = 96;
+    register int rv = 97;
+    volatile int vv = 98;
 
+    printf("Before setjmp:\n");
+    printf("gv=%d av=%d rv=%d vv=%d sv=%d\n", gv, av, rv, vv, sv);
 
-	printf("in f1()\n");
-	printf("gv=%d,av=%d,rv=%d,vv=%d,sv=%d",gv,av,rv,vv,sv);
-	if(setjmp(buf) == 0)
-	{
-		gv = 100;
-		av = 200;
-		rv = 300;
-		vv = 400;
-		sv = 500;
-		
-		longjmp(buf,1);
-	}
-	else{
-		printf("After long jump\n");
-		printf("gv=%d,sv=%d,rv=%d,vv=%d,sv=%d",gv,av,rv,vv,sv);
-	}
+    if(setjmp(buf) == 0)
+    {
+        gv = 100;
+        av = 200;
+        rv = 300;
+        vv = 400;
+        sv = 500;
+
+        longjmp(buf, 1);
+    }
+    else
+    {
+        printf("After longjmp:\n");
+        printf("gv=%d av=%d rv=%d vv=%d sv=%d\n", gv, av, rv, vv, sv);
+    }
 }
+
 int main()
 {
-	f1();
-	return 0;
+    f1();
+    return 0;
 }
