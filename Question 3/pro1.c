2c1a6d0510bb4bc2ef25b317a080a6e4c8857654
Apheksha B <apheksha.b@gmail.com>
2026-05-02 17:33:18 +0530

    Refactor pro1.c for error handling

--- Question 3/pro1.c
+++ Question 3/pro1.c
@@ -3,36 +3,48 @@
 #include<unistd.h>
 #include<string.h>
 #include<fcntl.h>
+#include<stdlib.h>
 
-int main(int argc,char *argv[])
-{
+int main(int argc,char *argv[]){
     DIR *d;
     struct dirent *p;
     char path[100];
     int fd;
-    int size;
+    off_t size;
+
+    if(argc != 2){
+        printf("Usage: %s directory\n", argv[0]);
+        return 1;
+    }
 
     d = opendir(argv[1]);
+    if(d == NULL){
+        perror("opendir");
+        return 1;
+    }
 
-    while((p = readdir(d)) != NULL)
-    {
-        if(strcmp(p->d_name,".")==0 || strcmp(p->d_name,"..")==0)
+    while((p = readdir(d)) != NULL){
+        if(strcmp(p->d_name,".") == 0 || strcmp(p->d_name,"..") == 0)
             continue;
 
         sprintf(path,"%s/%s",argv[1],p->d_name);
 
         fd = open(path,O_RDONLY);
+        if(fd < 0)
+            continue;
 
         size = lseek(fd,0,SEEK_END);
 
-        if(size==0)
-        {
-            unlink(path);
-            printf("Removed empty file: %s\n",path);
+        if(size == 0){
+            if(unlink(path) == 0)
+                printf("removed empty :%s\n",path);
+            else
+                perror("unlink");
         }
 
         close(fd);
     }
 
     closedir(d);
+    return 0;
 }
