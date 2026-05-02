fef236b66b324d442469215c47cb539bbfab36ab
Apheksha B <apheksha.b@gmail.com>
2026-05-02 17:47:15 +0530

    Enhance directory listing with detailed file info
    Refactor directory reading and file information display. Improved error handling for 'stat' and updated output formatting.

--- Question 3/pro2.c
+++ Question 3/pro2.c
@@ -2,33 +2,47 @@
 #include<dirent.h>
 #include<sys/stat.h>
 #include<time.h>
-#include<fcntl.h>
 #include<string.h>
+#include<stdlib.h>
 
 int main(int argc, char *argv[])
 {
-	DIR *dp;
-	struct dirent *d;
-	struct stat m;
-	char *dir;
-	char path[100];
+    DIR *dp;
+    struct dirent *d;
+    struct stat m;
+    char *dir;
+    char path[200];
 
-	dir = (argc > 1) ? argv[1] : ".";
-	dp = opendir(dir);
+    dir = (argc > 1) ? argv[1] : ".";
 
-	if(dp == NULL)
-	{
-		printf("Cannot open directory");
-		return 0;
-	}
+    dp = opendir(dir);
+    if(dp == NULL)
+    {
+        perror("opendir");
+        return 1;
+    }
 
-	while((d = readdir(dp)) != NULL)
-	{
-		sprintf(path, "%s%s", dir, d->d_name);
-		stat(path,&m);
+    printf("Inode\tMode\tUID\tGID\tAccess Time\t\tFile Name\n");
 
-		printf("%ld %o %d %d %s %s\n", m.st_ino, m.st_mode, m.st_uid, m.st_gid, ctime(&m.st_atime),d->d_name);
-	}
-	closedir(dp);
-	return 0;
+    while((d = readdir(dp)) != NULL)
+    {
+        sprintf(path, "%s/%s", dir, d->d_name);
+
+        if(stat(path, &m) == -1)
+        {
+            perror("stat");
+            continue;
+        }
+
+        printf("%ld\t%o\t%d\t%d\t%s\t%s\n",
+               (long)m.st_ino,
+               m.st_mode & 0777,
+               (int)m.st_uid,
+               (int)m.st_gid,
+               ctime(&m.st_atime),
+               d->d_name);
+    }
+
+    closedir(dp);
+    return 0;
 }
