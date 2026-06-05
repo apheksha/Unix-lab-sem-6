beddddbb6c0ae12268f4e056b3aed3b655f0cb95
Apheksha B <apheksha.b@gmail.com>
2026-06-05 21:30:34 +0530

    Fix formatting and improve output for arguments and env vars

--- Question 12/partb/echoall.c
+++ Question 12/partb/echoall.c
@@ -1,18 +1,26 @@
+// Program to display arguments and environment variables.
+
 #include<stdio.h>
 
 extern char **environ;
 
-int main(int argc, char *argv[])
+int main(int argc,char *argv[])
 {
     int i;
 
     printf("Arguments:\n");
-    for(i=0;i<argc;i++)
-        printf("argv[%d]=%s\n", i, argv[i]);
 
-    printf("\nEnvironment variables:\n");
-    for(i=0; environ[i]!=NULL; i++)
-        printf("%s\n", environ[i]);
+    for(i=0;i<argc;i++)
+    {
+        printf("argv[%d] = %s\n",i,argv[i]);
+    }
+
+    printf("\nEnvironment Variables:\n");
+
+    for(i=0;environ[i]!=NULL;i++)
+    {
+        printf("%s\n",environ[i]);
+    }
 
     return 0;
 }
