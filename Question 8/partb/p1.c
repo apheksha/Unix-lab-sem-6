27d49009d9323401a7b401374067fabf77ef5176
Apheksha B <apheksha.b@gmail.com>
2026-06-05 20:56:03 +0530

    Fix formatting and improve output messages in p1.c

--- Question 8/partb/p1.c
+++ Question 8/partb/p1.c
@@ -1,25 +1,29 @@
-#include <stdio.h>
-#include <unistd.h>
+// Child program used by parent process to demonstrate access() function.
 
-int main(int argc, char *argv[])
+#include<stdio.h>
+#include<unistd.h>
+
+int main(int argc,char *argv[])
 {
-    if(argc != 2)
+    if(argc!=2)
     {
-        printf("Usage: %s filename\n", argv[0]);
+        printf("Usage: %s <filename>\n",argv[0]);
         return 1;
     }
 
-    if(access(argv[1], F_OK) == 0)
-        printf("File exists\n");
+    if(access(argv[1],F_OK)==0)
+        printf("File Exists\n");
     else
-        printf("File does not exist\n");
+        printf("File Does Not Exist\n");
 
-    if(access(argv[1], R_OK) == 0)
-        printf("Read permission: YES\n");
-    if(access(argv[1], W_OK) == 0)
-        printf("Write permission: YES\n");
-    if(access(argv[1], X_OK) == 0)
-        printf("Execute permission: YES\n");
+    if(access(argv[1],R_OK)==0)
+        printf("Read Permission : YES\n");
+
+    if(access(argv[1],W_OK)==0)
+        printf("Write Permission : YES\n");
+
+    if(access(argv[1],X_OK)==0)
+        printf("Execute Permission : YES\n");
 
     return 0;
 }
