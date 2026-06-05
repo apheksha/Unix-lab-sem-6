a44214ef8e86daaf083bc8b071ab4b20d6b0b54a
Apheksha B <apheksha.b@gmail.com>
2026-06-05 19:52:34 +0530

    Fix argument validation and enhance link creation logic
    Updated argument checks and improved error handling for link creation.

--- Question 4/pro1.c
+++ Question 4/pro1.c
@@ -1,28 +1,35 @@
+// Write a C program to demonstrate the creation of soft links and hard links.
+
 #include<stdio.h>
-#include<stdlib.h>
 #include<unistd.h>
 
-int main(int argc, char *argv[])
+int main(int argc,char *argv[])
 {
-    if(argc == 4 && argv[1][0] == 'h')
+    if(argc!=4)
     {
-        if(link(argv[2], argv[3]) == 0)
-            printf("Hard link created\n");
+        printf("Usage:\n");
+        printf("%s h <source> <hardlink>\n",argv[0]);
+        printf("%s s <source> <softlink>\n",argv[0]);
+        return 1;
+    }
+
+    if(argv[1][0]=='h')
+    {
+        if(link(argv[2],argv[3])==0)
+            printf("Hard Link Created\n");
         else
             perror("link");
     }
-    else if(argc == 4 && argv[1][0] == 's')
+    else if(argv[1][0]=='s')
     {
-        if(symlink(argv[2], argv[3]) == 0)
-            printf("Soft link created\n");
+        if(symlink(argv[2],argv[3])==0)
+            printf("Soft Link Created\n");
         else
             perror("symlink");
     }
     else
     {
-        printf("Usage:\n");
-        printf("%s h <source> <hardlink>\n", argv[0]);
-        printf("%s s <source> <softlink>\n", argv[0]);
+        printf("Invalid Option\n");
     }
 
     return 0;
