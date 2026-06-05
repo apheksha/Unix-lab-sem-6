820871e83cdfb5f772434275df81743e820046a8
Apheksha B <apheksha.b@gmail.com>
2026-06-05 21:25:03 +0530

    Improve formatting and structure in parent.c
    Refactor code for clarity and consistency.

--- Question 11/partb/parent.c
+++ Question 11/partb/parent.c
@@ -1,31 +1,36 @@
+// Write a program to perform the following operations:
+// i. To create a child process
+// ii. The child process should execute a separate program (using exec function) that calculates the addition of two numbers by passing two integer values.
+// iii. The parent process should wait for a child to complete
+
 #include<stdio.h>
-#include<unistd.h>
 #include<stdlib.h>
+#include<unistd.h>
 #include<sys/wait.h>
 
-int main(int argc, char *argv[])
+int main(int argc,char *argv[])
 {
-    int pid, status;
+    pid_t pid;
+    int status;
 
-    if(argc != 3)
+    if(argc!=3)
     {
-        printf("Usage: %s num1 num2\n", argv[0]);
+        printf("Usage: %s <num1> <num2>\n",argv[0]);
         return 1;
     }
 
-    pid = fork();
+    pid=fork();
 
-    if(pid == 0)
+    if(pid==0)
     {
-        execl("./child", "child", argv[1], argv[2], NULL);
+        execl("./child","child",argv[1],argv[2],NULL);
         perror("execl");
         exit(1);
     }
-    else
-    {
-        wait(&status);
-        printf("Child exited with status: %d\n", WEXITSTATUS(status));
-    }
+
+    wait(&status);
+
+    printf("Child Completed\n");
 
     return 0;
 }
