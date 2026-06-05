e58c66f3603eb102d7bf18f11f9602512f7b28cf
Apheksha B <apheksha.b@gmail.com>
2026-06-05 20:06:57 +0530

    Update SIGINT handler to print messages and reset action
    Refactor signal handler for SIGINT to print messages and reset action.

--- Question 5/pro2.c
+++ Question 5/pro2.c
@@ -1,38 +1,39 @@
+// Write a C program using sigaction system call which calls a signal handler on SIGINT signal and then reset the default action of the SIGINT signal.
+
 #include<stdio.h>
-#include<stdlib.h>
 #include<signal.h>
 #include<unistd.h>
 
 void handler(int signo)
 {
-    printf("\nSIGINT caught. Next Ctrl+C will terminate.\n");
-
     struct sigaction sa;
-    sa.sa_handler = SIG_DFL;          
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
 
-    sigaction(SIGINT, &sa, NULL);
+    printf("\nSIGINT Caught\n");
+    printf("Next Ctrl+C Will Terminate Program\n");
+
+    sa.sa_handler=SIG_DFL;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags=0;
+
+    sigaction(SIGINT,&sa,NULL);
 }
 
 int main()
 {
     struct sigaction sa;
 
-    sa.sa_handler = handler;
+    sa.sa_handler=handler;
     sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
+    sa.sa_flags=0;
 
-    if(sigaction(SIGINT, &sa, NULL) < 0)
-    {
-        perror("sigaction");
-        return 1;
-    }
+    sigaction(SIGINT,&sa,NULL);
 
-    printf("Press Ctrl+C to trigger SIGINT\n");
+    printf("Press Ctrl+C\n");
 
     while(1)
-        pause();   
+    {
+        pause();
+    }
 
     return 0;
 }
