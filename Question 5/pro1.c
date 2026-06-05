802b3729a330cb5250259fba4ed518350a47fa87
Apheksha B <apheksha.b@gmail.com>
2026-06-05 20:05:30 +0530

    Refactor file time copy program to use command line args

--- Question 5/pro1.c
+++ Question 5/pro1.c
@@ -1,36 +1,36 @@
+// Write a program to copy access and modification time of a file to another file using utime function.
+
 #include<stdio.h>
 #include<sys/stat.h>
 #include<utime.h>
-#include<stdlib.h>
 
-int main()
+int main(int argc,char *argv[])
 {
-    char file1[100], file2[100];
-    struct stat s;
-    struct utimbuf t;
+    struct stat st;
+    struct utimbuf timebuf;
 
-    printf("Enter source file: ");
-    scanf("%s", file1);
+    if(argc!=3)
+    {
+        printf("Usage: %s <sourcefile> <destinationfile>\n",argv[0]);
+        return 1;
+    }
 
-    printf("Enter destination file: ");
-    scanf("%s", file2);
-
-    if(stat(file1, &s) == -1)
+    if(stat(argv[1],&st)==-1)
     {
         perror("stat");
         return 1;
     }
 
-    t.actime = s.st_atime;
-    t.modtime = s.st_mtime;
+    timebuf.actime=st.st_atime;
+    timebuf.modtime=st.st_mtime;
 
-    if(utime(file2, &t) == -1)
+    if(utime(argv[2],&timebuf)==-1)
     {
         perror("utime");
         return 1;
     }
 
-    printf("Time copied successfully\n");
+    printf("Time Copied Successfully\n");
 
     return 0;
 }
