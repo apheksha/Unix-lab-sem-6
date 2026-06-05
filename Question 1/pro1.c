8fc31f21f9f63e77facd8bc246a2cf5721758afa
Apheksha B <apheksha.b@gmail.com>
2026-06-05 19:22:15 +0530

    Refactor file reading to use command line argument

--- Question 1/pro1.c
+++ Question 1/pro1.c
@@ -1,34 +1,49 @@
-#include<stdio.h>
-#include<fcntl.h>
-#include<unistd.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-int main(){
-    char file[100], ch;
+int main(int argc, char *argv[])
+{
     int fd;
+    char ch;
     off_t size, i;
 
-    printf("Enter the filename: ");
-    scanf("%s", file);
+    if (argc != 2)
+    {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
 
-    fd = open(file, O_RDONLY);
+    fd = open(argv[1], O_RDONLY);
 
-    if(fd < 0){
+    if (fd < 0)
+    {
         perror("open");
         return 1;
     }
 
     size = lseek(fd, 0, SEEK_END);
 
+    if (size == -1)
+    {
+        perror("lseek");
+        close(fd);
+        return 1;
+    }
+
     printf("Reverse Content:\n");
 
-    for(i = size - 1; i >= 0; i--){
+    for (i = size - 1; i >= 0; i--)
+    {
         lseek(fd, i, SEEK_SET);
-		read(fd, &ch, 1);
-		printf("%c", ch);
+        read(fd, &ch, 1);
+        write(STDOUT_FILENO, &ch, 1);
     }
 
     printf("\n");
+
     close(fd);
+
     return 0;
 }
