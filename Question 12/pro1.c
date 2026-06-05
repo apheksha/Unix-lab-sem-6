3b0b654aa0ca606997ad528fea11235079f6023e
Apheksha B <apheksha.b@gmail.com>
2026-06-05 21:28:01 +0530

    Update pro1.c

--- Question 12/pro1.c
+++ Question 12/pro1.c
@@ -1,24 +1,36 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/wait.h>
+// Write a program to demonstrate the zombie state of a process and provide the solution for the same.
 
-int main(void) {
-    pid_t pid = fork();
-    if (pid < 0) { perror("fork"); return 1; }
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/wait.h>
 
-    if (pid == 0) {
-        printf("Child exiting. PID: %d\n", getpid());
+int main()
+{
+    pid_t pid;
+
+    pid=fork();
+
+    if(pid<0)
+    {
+        perror("fork");
+        return 1;
+    }
+
+    if(pid==0)
+    {
+        printf("Child Exiting. PID : %d\n",getpid());
         exit(0);
     }
 
-    printf("Parent sleeping 10s. Child (PID %d) is now zombie.\n", pid);
-    printf("Run: ps -l | grep Z\n");
+    printf("Child PID %d is Zombie for 10 Seconds\n",pid);
+    printf("Run : ps -l | grep Z\n");
 
-    sleep(10);   // Zombie exists here
+    sleep(10);
 
-    wait(NULL);  // Solution
-    printf("Zombie removed using wait()\n");
+    wait(NULL);
+
+    printf("Zombie Removed Using wait()\n");
 
     return 0;
 }
