76c77df92462b02049ae620a2005bce05e434717
Apheksha B <apheksha.b@gmail.com>
2026-06-05 21:04:23 +0530

    Refactor file reading operations in pro2.c

--- Question 9/pro2.c
+++ Question 9/pro2.c
@@ -1,43 +1,55 @@
+// Write a C program to perform the following operations:
+// i. To read the first 20 characters from a file
+// ii. seek to 10th byte from the beginning and display 20 characters from there
+// iii. seek 10 bytes ahead from the current file offset and display 20 characters
+// iv. Display the file size
+
 #include<stdio.h>
 #include<unistd.h>
 #include<fcntl.h>
 
-int main(int argc, char *argv[])
+int main(int argc,char *argv[])
 {
     int fd;
-    char buf[21];
     int n;
+    char buf[21];
+    off_t size;
 
-    if(argc != 2)
+    if(argc!=2)
     {
-        printf("Usage: %s filename\n", argv[0]);
+        printf("Usage: %s <filename>\n",argv[0]);
         return 1;
     }
 
-    fd = open(argv[1], O_RDONLY);
-    if(fd < 0)
+    fd=open(argv[1],O_RDONLY);
+
+    if(fd<0)
     {
         perror("open");
         return 1;
     }
 
-    n = read(fd, buf, 20);
-    buf[n] = '\0';
-    printf("First 20 chars: %s\n", buf);
+    n=read(fd,buf,20);
+    buf[n]='\0';
+    printf("First 20 Characters : %s\n",buf);
 
-    lseek(fd, 10, SEEK_SET);
-    n = read(fd, buf, 20);
-    buf[n] = '\0';
-    printf("20 chars from 10th byte: %s\n", buf);
+    lseek(fd,10,SEEK_SET);
 
-    lseek(fd, 10, SEEK_CUR);
-    n = read(fd, buf, 20);
-    buf[n] = '\0';
-    printf("20 chars from current+10: %s\n", buf);
+    n=read(fd,buf,20);
+    buf[n]='\0';
+    printf("From 10th Byte : %s\n",buf);
 
-    off_t size = lseek(fd, 0, SEEK_END);
-    printf("File size: %ld bytes\n", (long)size);
+    lseek(fd,10,SEEK_CUR);
+
+    n=read(fd,buf,20);
+    buf[n]='\0';
+    printf("Current + 10 : %s\n",buf);
+
+    size=lseek(fd,0,SEEK_END);
+
+    printf("File Size : %ld Bytes\n",(long)size);
 
     close(fd);
+
     return 0;
 }
