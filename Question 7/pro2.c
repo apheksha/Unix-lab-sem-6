4c853d503927fa045cee6bf7b9959ed8302de454
Apheksha B <apheksha.b@gmail.com>
2026-06-05 20:48:28 +0530

    Refactor file copy to use open/read/write
    Updated file handling to use low-level I/O functions for copying.

--- Question 7/pro2.c
+++ Question 7/pro2.c
@@ -1,37 +1,61 @@
-#include<stdio.h>
+// C program to simulate copy command by accepting filenames
+// from command line and reporting errors.
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int main(int argc, char *argv[])
 {
-    FILE *fp1,*fp2;
-    int ch;
+    int src, dest;
+    char buf[1024];
+    ssize_t n;
 
-    if(argc != 3)
+    if (argc != 3)
     {
-        printf("Usage: %s source destination\n",argv[0]);
+        printf("Usage: %s <source_file> <destination_file>\n", argv[0]);
         return 1;
     }
 
-    fp1 = fopen(argv[1],"r");
-    if(fp1 == NULL)
+    src = open(argv[1], O_RDONLY);
+
+    if (src < 0)
     {
-        perror("Source file");
+        perror("Source File");
         return 1;
     }
 
-    fp2 = fopen(argv[2],"w");
-    if(fp2 == NULL)
+    dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+    if (dest < 0)
     {
-        perror("Destination file");
+        perror("Destination File");
+        close(src);
         return 1;
     }
 
-    while((ch = fgetc(fp1)) != EOF)
-        fputc(ch,fp2);
+    while ((n = read(src, buf, sizeof(buf))) > 0)
+    {
+        if (write(dest, buf, n) != n)
+        {
+            perror("Write Error");
+            close(src);
+            close(dest);
+            return 1;
+        }
+    }
 
-    printf("Copied successfully\n");
+    if (n < 0)
+    {
+        perror("Read Error");
+    }
+    else
+    {
+        printf("File Copied Successfully\n");
+    }
 
-    fclose(fp1);
-    fclose(fp2);
+    close(src);
+    close(dest);
 
     return 0;
 }
