050635fc9ca0f8e82c6c5e5035b84935590ff0d4
Apheksha B <apheksha.b@gmail.com>
2026-06-05 20:57:21 +0530

    Refactor child process creation and argument passing
    Refactor main function to improve readability and correct argument handling.

--- Question 8/partb/main.c
+++ Question 8/partb/main.c
@@ -1,35 +1,48 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <stdlib.h>
+// Write a C program to perform the following operations:
+// i. To create a child process.
+// ii. The child process should execute a program (using exec()) to show the use of the access function.
+// iii. The parent process should wait for the child process to exit.
+// iv. Also print the necessary process IDs.
 
-int main()
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/wait.h>
+
+int main(int argc,char *argv[])
 {
-    pid_t pid = fork();
+    pid_t pid;
 
-    if(pid < 0)
+    if(argc!=2)
+    {
+        printf("Usage: %s <filename>\n",argv[0]);
+        return 1;
+    }
+
+    pid=fork();
+
+    if(pid<0)
     {
         perror("fork");
         return 1;
     }
 
-    if(pid == 0)
+    if(pid==0)
     {
-        printf("Child PID: %d Parent PID: %d\n", getpid(), getppid());
+        printf("Child PID  : %d\n",getpid());
+        printf("Parent PID : %d\n",getppid());
 
-        execl("./p1", "p1", "example.txt", (char *)NULL);
+        execl("./p1","p1",argv[1],NULL);
 
         perror("execl");
         exit(1);
     }
-    else
-    {
-        printf("Parent PID: %d waiting for child %d\n", getpid(), pid);
 
-        wait(NULL);
+    printf("Parent PID : %d\n",getpid());
 
-        printf("Child process completed\n");
-    }
+    wait(NULL);
+
+    printf("Child Process Completed\n");
 
     return 0;
 }
