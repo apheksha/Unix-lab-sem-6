dcb01d0c12aa79d336ab0b455d4bc9a9a417f2e4
Apheksha B <apheksha.b@gmail.com>
2026-06-05 19:53:31 +0530

    Refactor echoall.c for improved argument printing

--- Question 4/partb/echoall.c
+++ Question 4/partb/echoall.c
@@ -1,13 +1,21 @@
-#include<stdio.h>
-#include<stdlib.h>
+// Write a C program:
+// i. To create a child process.
+// ii. The child should execute an interpreter file by passing a few arguments.
+// iii. Create an interpreter file that has the path of echoall.c file and pass one argument.
+// iv. Create echoall.c file which prints the arguments received from both child process and interpreter file.
 
-int main(int argc, char *argv[])
+#include<stdio.h>
+
+int main(int argc,char *argv[])
 {
-	int i;
-	printf("Arguments received by echoall:\n");
-	for(i = 0; i<argc;i++)
-	{
-		printf("argv[%d] = %s\n", i, argv[i]);
-	}
-	return 0;
+    int i;
+
+    printf("Arguments Received:\n");
+
+    for(i=0;i<argc;i++)
+    {
+        printf("argv[%d] = %s\n",i,argv[i]);
+    }
+
+    return 0;
 }
