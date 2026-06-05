220b0f86339e6818918ca063e229236cadc6f2c8
Apheksha B <apheksha.b@gmail.com>
2026-06-05 21:30:56 +0530

    Refactor process creation and execution logic
    Fix error handling for fork and update exec calls.

--- Question 12/partb/main.c
+++ Question 12/partb/main.c
@@ -1,3 +1,8 @@
+// Write a C program to perform the following operations:
+// i. To create a child and parent process with the use of an echoall file.
+// ii. The Child should execute a process that prints the user defined values of environment variables.
+// iii. The Parent should execute a process that prints default values for the environment variables.
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
@@ -9,22 +14,29 @@ int main()
 {
     pid_t pid;
 
-    pid = fork();
+    pid=fork();
 
-    if(pid == 0)
+    if(pid<0)
     {
-        char *env[] = {"USER=Student", "CITY=India", NULL};
-        execle("./echoall", "echoall", "child", NULL, env);
+        perror("fork");
+        return 1;
+    }
+
+    if(pid==0)
+    {
+        char *env[]={"USER=Student","CITY=India",NULL};
+
+        execle("./echoall","echoall","child",NULL,env);
+
         perror("execle");
         exit(1);
     }
-    else
-    {
-        wait(NULL);
 
-        execve("./echoall", (char*[]){"echoall","parent",NULL}, environ);
-        perror("execve");
-    }
+    wait(NULL);
+
+    execve("./echoall",(char *[]){"echoall","parent",NULL},environ);
+
+    perror("execve");
 
     return 0;
 }
